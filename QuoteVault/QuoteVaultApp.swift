98a2cd9be5a9cb1dcf95ac76515f818639368ae9
Leticia Bezerra <leticiabezerrauy@gmail.com>
2026-01-21 15:56:35 -0300

    first commit

---
+++ QuoteVault/QuoteVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteVaultApp.swift
+//  QuoteVault
+//
+//  Created by Leticia Bezerra on 21/01/26.
+//
+
+import SwiftUI
+
+@main
+struct QuoteVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
